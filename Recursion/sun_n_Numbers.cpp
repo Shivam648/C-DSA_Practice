58e0c81afecb3f109d354d1c1dc3647fbcfca82b
Shivam <shivamjaiswal648@gmail.com>
2022-09-30 20:21:22 +0530

    Sum of n

--- Recursion/sun_n_Numbers.cpp
+++ Recursion/sun_n_Numbers.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 int sum(int n)
@@ -7,6 +7,22 @@ int sum(int n)
         return 1;
     int partialAns = sum(n - 1);
     return n + partialAns;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    cout << sum(n);
+} */
+
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    if (n == 1)
+        return 1;
+    return n + sum(n - 1);
 }
 int main()
 {
