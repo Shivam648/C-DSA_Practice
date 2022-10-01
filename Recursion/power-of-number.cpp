98094dd7c9c47a918d3dae5cedcd4e0c86162904
Shivam <shivamjaiswal648@gmail.com>
2022-10-01 12:27:05 +0530

    Optimised: Power of Number

--- Recursion/power-of-number.cpp
+++ Recursion/power-of-number.cpp
@@ -1,17 +1,61 @@
+/* Implement pow(x, n), which calculates x raised to the power n(i.e., xn).
+
+                     Example 1 :
+
+    Input : x = 2.00000,
+            n = 10 Output : 1024.00000 Example 2 :
+
+    Input : x = 2.10000,
+            n = 3 Output : 9.26100 Example 3 :
+
+    Input : x = 2.00000,
+            n = -2 Output : 0.25000 Explanation : 2 - 2 = 1 / 22 = 1 / 4 = 0.25
+ */
+
 #include <iostream>
 using namespace std;
 
-long long pow(int x, int n)
+/* long long pow(int x, int n)
 {
     if (n == 0)
         return 1;
     int partialAns = pow(x, n - 1);
     return x * partialAns;
+} */
+
+double pow(double x, long int n)
+{
+    int sign = 0;
+    if (n < 0)
+    {
+        sign = 1;
+        n = n * -1;
+    }
+    double ans = 1.0;
+    while (n)
+    {
+        if (n % 2)
+        {
+            ans = ans * x;
+            n = n - 1;
+        }
+        else
+        {
+            x = x * x;
+            n = n / 2;
+        }
+    }
+    if (sign)
+    {
+        ans = (double)1 / ans;
+    }
+    return ans;
 }
 
 int main()
 {
-    int x, n;
+    double x;
+    long int n;
     cout << "Enter the value of x" << endl;
     cin >> x;
     cout << "Enter the value of n" << endl;
